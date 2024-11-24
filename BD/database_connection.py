271e10976fdd86dc34f815e2222ab0cc89d8ffb8
DamienRabier <damrabierpro@gmail.com>
2024-11-24 17:44:54 +0100

    add script sql and python orm

---
+++ BD/database_connection.py
@@ -0,0 +1,9 @@
+password = "password"
+username = "postgres"
+database = "postgres"
+host = "localhost"
+port = 5432
+
+
+# TODO modify with your database URL
+DATABASE_URL = f"postgresql+psycopg2://{username}:{password}@{host}:{port}/{database}"
