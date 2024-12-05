6fbd3952a2e44d1b90ee47aae95727de18fac86b
MaxRonce <max.ron@outlook.fr>
2024-12-05 09:52:46 +0100

    FEAT - edit readme

--- src/main.py
+++ src/main.py
@@ -6,11 +6,26 @@ from pipeline.pipeline import DataPipeline
 from database.operations import DatabaseConnector
 from database.connection import DATABASE_URL
 
+import numpy as np
+import pandas as pd
+from astropy.io import fits
+
+import os
 
 if __name__ == "__main__":
     start = time.time()
-    # Load dataset
-    df = pd.read_csv('../data/allStarLite_10rows.csv')
+
+    df_path = '../data/allStarLiteFULL.csv'
+
+    if not os.path.exists(df_path):
+        print("File not found, creating it")
+        hdulist = fits.open('data/allStarLite-dr17-synspec_rev1.fits')
+        hdu1_data = hdulist[1].data
+        df = pd.DataFrame(hdu1_data.tolist(), columns=hdu1_data.names)
+    else :
+        print("File found, loading it")
+        df = pd.read_csv(df_path)
+
     print(f"Dataset loaded, time taken: {time.time() - start:.2f} seconds")
 
 
